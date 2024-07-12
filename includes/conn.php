13846b5bb03c2380b97d952d048b5f454b145d3d
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-12 19:00:38 +0800

    New changes

--- includes/conn.php
+++ includes/conn.php
@@ -3,13 +3,14 @@
 $host = "127.0.0.1:3306";
 $dbname = "u510162695_votesystem";
 $username = "u510162695_votesystem";
-$password = "1Votesystem123";
+$password = "1Votesystem";
 
 //create connection
 	$conn = new mysqli($host, $username, $password, $dbname);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
+		echo "Connection Failed";
 	}
-	echo "Connected";
+	echo "Connection Success";
 ?>
