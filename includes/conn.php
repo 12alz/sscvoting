742bc71a23822d007dc0ca14d4fca4aca2278150
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-12 18:22:26 +0800

    Change remote connection

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('u510162695_', 'u510162695_', '1Votesystem', 'u510162695_');
+	$conn = new mysqli('localhost', 'u510162695_', '1Votesystem', 'u510162695_');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
