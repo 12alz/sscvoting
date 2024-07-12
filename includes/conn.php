7a6cace21c60ea90f5ba7a426c87674aa9230027
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-12 18:53:47 +0800

    New changes

--- includes/conn.php
+++ includes/conn.php
@@ -1,13 +1,13 @@
 <?php
 echo "hello world";
 // database credential
-$host = "localhost";
+$host = "127.0.0.1:3306";
 $dbname = "u510162695_votesystem";
 $username = "u510162695_votesystem";
 $password = "1Votesystem";
 
 //create connection
-	$conn = new mysqli($host, $username, $password, $dbname);
+	$conn = new mysqli($host, $port, $username, $password, $dbname);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
