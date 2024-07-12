6754568514bedd799e559b2aa55ead8eb8672ef1
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-12 18:13:23 +0800

    Change Database Connection

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('u510162695_', 'u510162695_', '1Votesystem', 'u510162695_');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
