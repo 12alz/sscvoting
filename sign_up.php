a8f4233f0154a9a82f7c4ecf47a67c3d187270c8
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-14 21:31:38 +0800

    change

--- sign_up.php
+++ sign_up.php
@@ -3,7 +3,7 @@
     include 'includes/conn.php';
 
     if(isset($_POST['add']) && $_POST['g-recaptcha-response'] != "") {
-    $secret = '6LdRBqsfAAAAAIwmnnQLsBC6n3mdQHVluhhmeHz2';
+    $secret = '6LcczA8qAAAAADhlTmRbkweOTae2QbXux0mlhW8m';
     $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
     $responseData = json_decode($verifyResponse);
     if ($responseData->success) {
