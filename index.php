a7afef5f96549ccd6cce9b803afaeca06e13cf53
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-20 00:23:16 +0800

    change

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ if(isset($_SESSION['voter'])){
 <body>
     <div class="login-box">
         <div class="login-logo">
-        <img src="images/jerson.png">
+        <img src="images/1.png">
         <h2>Supreme Student Council Voting System </h2>
         </div>
         <div class="login-box-body">
