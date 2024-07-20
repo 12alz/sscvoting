3def00b9e525e7e5e0ec86841972d6f87e4674c4
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-20 22:05:04 +0800

    change

--- admin/voters_edit.php
+++ admin/voters_edit.php
@@ -1,56 +1,25 @@
 <?php
-include 'includes/session.php';
+	include 'includes/session.php';
 
-if(isset($_POST['edit'])){
-    $id = $_POST['id'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $course = $_POST['course'];
+	if(isset($_POST['edit'])){
+		$id = $_POST['id'];
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		$course = $_POST['course'];
 
-    $sql = "UPDATE voters SET firstname = '$firstname', lastname = '$lastname', password = '$password', course = '$course' WHERE id = '$id'";
-    if($conn->query($sql)){
-        $_SESSION['success'] = 'Voter updated successfully';
+		$sql = "UPDATE voters SET firstname = '$firstname', lastname = '$lastname', password = '$password' , course = '$course' WHERE id = '$id'";
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Voter updated successfully';
+		}
+		else{
+			$_SESSION['error'] = $conn->error;
+		}
+		}
+	else{
+		$_SESSION['error'] = 'Voter Updated Error';
+	}
 
-        // Use SweetAlert for success message
-        echo "<script>
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Success!',
-                    text: 'Voter updated successfully.',
-                }).then(function() {
-                    window.location = 'voters.php'; // Redirect to voters.php after closing alert
-                });
-              </script>";
-    }
-    else{
-        $_SESSION['error'] = $conn->error;
-
-        // Use SweetAlert for error message
-        echo "<script>
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error!',
-                    text: 'Error occurred: " . $conn->error . "',
-                }).then(function() {
-                    window.location = 'voters.php'; // Redirect to voters.php after closing alert
-                });
-              </script>";
-    }
-}
-else{
-    $_SESSION['error'] = 'Voter update error';
-
-    // Use SweetAlert for error message
-    echo "<script>
-            Swal.fire({
-                icon: 'error',
-                title: 'Error!',
-                text: 'An error occurred during the voter update process.',
-            }).then(function() {
-                window.location = 'voters.php'; // Redirect to voters.php after closing alert
-            });
-          </script>";
-}
+	header('location: voters.php');
 
 ?>
