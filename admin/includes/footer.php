fa4097a852084fdb2d1d7d62080ca38b22845926
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-20 17:04:41 +0800

    change

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>version 2.0.0 SSC Voting System</b>
+      <b>version 2.0.0</b>
     </div>
     <strong> &copy; 2024 Brought To You By <a href="#">BSIT STUDENT</a></strong>
 </footer>
