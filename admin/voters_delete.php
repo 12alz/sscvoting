2beb4f0a69e4f2e37fbb245ef51be705e3c8cb85
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-22 21:36:21 +0800

    change

--- admin/voters_delete.php
+++ admin/voters_delete.php
@@ -4,20 +4,8 @@
 	if(isset($_POST['delete'])){
 		$id = $_POST['id'];
 		$sql = "UPDATE voters SET recstat = 1 WHERE id = '$id'";
-		if($conn->query($sql)){
-			echo "
-			 <script>
-                        Swal.fire({
-                        timer: '1500', 
-                            icon: 'success',
-                            title: 'The records was successfully archived',
-                            text: '{$_SESSION['success']}',
-                        }).then(function() {
-                            window.location = 'voters.php';
-                        });
-                    </script>
-                ";
-			// $_SESSION['success'] = 'The records was successfully archived';
+		if($conn->query($sql)){z
+			$_SESSION['success'] = 'The records was successfully archived';
 		}
 		else{
 			$_SESSION['error'] = $conn->error;
