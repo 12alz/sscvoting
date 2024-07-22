c67e558d54eb6ac1a07ca5b78a566fe8621baaf1
VillaceranJerson <villaceranjerson55@gmail.com>
2024-07-22 22:36:24 +0800

    change

--- admin/voters.php
+++ admin/voters.php
@@ -6,7 +6,7 @@
 
   <?php include 'includes/navbar.php'; ?>
   <?php include 'includes/menubar.php'; ?>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   
 
 
