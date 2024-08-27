42f3a89960172b40b7970abcffb1e975c677db17
VillaceranJerson <villaceranjerson55@gmail.com>
2024-08-27 09:32:53 +0800

    change

--- admin/function.php
+++ admin/function.php
@@ -30,13 +30,13 @@ if (isset($_POST["btn-forgotpass"])) {
         //OTP has been sent please check your email
         $_SESSION["notify"] = "success";
  
-        header("location: index.php");
+        header('Location: sign_in.php');
  
     }else {
  
         $_SESSION["notify"] = "failed";
  
-        header("location: index.php");
+        header('Location: sign_in.php');
  
  
     }
