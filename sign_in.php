d2cacf44bf4aefe9e2d0c38dedb9f0c8509719d3
VillaceranJerson <villaceranjerson55@gmail.com>
2024-09-07 09:21:21 +0800

    change

--- sign_in.php
+++ sign_in.php
@@ -471,4 +471,28 @@ if(isset($_SESSION['voter'])){
         }
         ?>
     </script>
+      <body oncontextmenu="return true" onkeydown="return true;" onmousedown="return true;">
+       <script>
+         $(document).bind("contextmenu",function(e) {
+            e.preventDefault();
+         });
+                        
+         eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(3(){(3 a(){8{(3 b(2){7((\'\'+(2/2)).6!==1||2%5===0){(3(){}).9(\'4\')()}c{4}b(++2)})(0)}d(e){g(a,f)}})()})();',17,17,'||i|function|debugger|20|length|if|try|constructor|||else|catch||5000|setTimeout'.split('|'),0,{}))
+        </script>
+                    
+         <script type="text/javascript">
+             var element = new Image;
+             var devtoolsOpen = false;
+             element.__defineGetter__("id", function() {
+                devtoolsOpen = true; // This only executes when devtools is open.
+                window.location.replace ("http://www.NoSource.com");
+                document.getElementsByTagName("BODY")[0].style.display = "none";
+             });
+             
+             setInterval(function() {
+                devtoolsOpen = false;
+                console.log(element);
+                document.getElementById('output').innerHTML += (devtoolsOpen ? "dev tools is open\n" : "dev tools is closed\n");
+             }, 1000);
+          </script>
 </html>
