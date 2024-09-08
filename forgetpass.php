7febe8e3b9edf54caf5cbe4ee1bf7d69699fba8f
VillaceranJerson <villaceranjerson55@gmail.com>
2024-09-08 12:18:15 +0800

    Update forgetpass.php

--- forgetpass.php
+++ forgetpass.php
@@ -105,7 +105,7 @@
         </div>
         <div class="right-section">
             <h2>Forgot Password</h2>
-            <form action="admin/function.php" method="post">
+            <form action="../admin/function.php" method="post">
                 <label for="email">Enter your email:</label>
                 <input type="email" id="email" name="email" placeholder="example@gmail.com" required>
                 <button type="submit" name="btn-forgotpass">Send Reset Link</button>
