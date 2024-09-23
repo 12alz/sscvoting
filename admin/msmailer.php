5b6b31ebe90759bfc158cb88138ba49bbe70fdff
VillaceranJerson <villaceranjerson55@gmail.com>
2024-09-23 22:36:08 +0800

    Update msmailer.php

--- admin/msmailer.php
+++ admin/msmailer.php
@@ -28,8 +28,8 @@ if (isset($_POST["btn-forgotpass"])) {
             $mail->Port = 587;
             $mail->SMTPSecure = 'tls';
             $mail->SMTPAuth = true;
-            $mail->Username = 'zilongs12@outlook.com'; // Your email address
-            $mail->Password = 'alexandre@gmail.com'; // Your email password
+            $mail->Username = 'kamatyanun@outlook.com'; // Your email address
+            $mail->Password = 'alexandre@123'; // Your email password
             $mail->setFrom('jeffreycahutay@outlook.com');
             $mail->addAddress($username); // Email address to send to
             $mail->isHTML(true);
