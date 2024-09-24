5cc24774a73c99bbf84b49750fee87fe70d22dc1
VillaceranJerson <villaceranjerson55@gmail.com>
2024-09-24 21:56:28 +0800

    update

--- contacts.php
+++ contacts.php
@@ -5,6 +5,7 @@
     <title>Contacts</title>
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="keywords" content="intense web design multipurpose template">
--- sign_in.php
+++ sign_in.php
@@ -18,6 +18,7 @@ if(isset($_SESSION['voter'])){
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="keywords" content="intense web design multipurpose template">
