28465b7e634cd463453eda8ac37ff4eae17a708f
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-03 12:11:06 +0800

    New

--- includes/conn.php
+++ includes/conn.php
@@ -14,32 +14,5 @@ $password = "1Votesystem";
 	}
 
 
-// Function to export the database
-function ExportDatabase($host, $user, $pass, $name)
-{
-    $mysqli = new mysqli($host, $user, $pass, $name);
-    $mysqli->select_db($name);
-    $mysqli->query("SET NAMES 'utf8'");
-
-    $queryTables = $mysqli->query('SHOW TABLES');
-    $targetTables = [];
-
-    while ($row = $queryTables->fetch_row()) {
-        $targetTables[] = $row[0];
-    }
-
-    // ... (fetch data from tables and create SQL content)
-
-    // Set appropriate headers for download
-    $backupName = $name . '.sql';
-    header('Content-Type: application/octet-stream');
-    header("Content-Transfer-Encoding: Binary");
-    header("Content-disposition: attachment; filename=\"$backupName\"");
-    echo $content;
-    exit;
-}
-
-// Usage
-ExportDatabase($host, $username, $password, $dbname);
-
+	echo "Connection Success"; 
 ?>
