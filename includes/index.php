23b4bcc9f5abb5cc051a73c41a3ccdf5b24ae134
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-03 12:21:08 +0800

    New

---
+++ includes/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   <link href="libs/css/bootstrap.3.3.6.min.css" rel="stylesheet">
+   <style>
+   body{
+     padding:10px 15px;
+   }
+   </style>
+   <title>Backup / Export Database</title>
+</head>
+<body>
+   <h1>Backup / Export Database</h1>
+   <div>
+     <p>Proses backup atau restore database mysql.</p>
+     <p>Silahkan klik tombol backup untuk mengunduh file .sql hasil backup dari database</p>
+     <p><a href="backup.php" class="btn btn-primary">Backup Database <span class="glyphicon glyphicon-save"></span></a></p>
+   </div>
+</body>
+</html>
