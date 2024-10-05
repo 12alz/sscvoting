b068f86e119e766e8b4ba695c5b6c5de73dfeb9c
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-05 14:11:30 +0800

    New Changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <!-- Site Title-->
     <title>Home</title>
     <meta charset="utf-8">
-    <meta name="format-detection" content="telephone=no">
+    <!-- <meta name="format-detection" content="telephone=no"> -->
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
