b49dd2e48d19ee1143bbe8af67675386289d5cf3
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-06 18:23:13 +0800

    Removed

--- admin/login.php
+++ admin/login.php
@@ -6,16 +6,17 @@ include 'includes/conn.php';
 $max_attempts = 3;
 $delay_time = 1000000; 
 
-if (!isset($_SESSION['login_attempts'])) {
-    $_SESSION['login_attempts'] = 0;
-    $_SESSION['last_attempt_time'] = 0;
-}
+// if (!isset($_SESSION['login_attempts'])) {
+//     $_SESSION['login_attempts'] = 0;
+//     $_SESSION['last_attempt_time'] = 0; bat naka session lods 
+// }
+// sure ka hydra lods? kasi solution jan eto lang rewrite koto code mo  at yung user and pass mo update mo lang gawin mo malakas
 
-if (time() - $_SESSION['last_attempt_time'] < $delay_time && $_SESSION['login_attempts'] >= $max_attempts) {
-    $_SESSION['error'] = 'Too many failed login attempts. Please try again after ' . ($delay_time - (time() - $_SESSION['last_attempt_time'])) . 'seconds.';
-    header('location: ../sign_in.php');
-    exit();
-}
+// if (time() - $_SESSION['last_attempt_time'] < $delay_time && $_SESSION['login_attempts'] >= $max_attempts) {
+//     $_SESSION['error'] = 'Too many failed login attempts. Please try again after ' . ($delay_time - (time() - $_SESSION['last_attempt_time'])) . 'seconds.';
+//     header('location: ../sign_in.php');
+//     exit();
+// }
 
 if (isset($_POST['login'])) {
     // Sanitize input
@@ -30,21 +31,21 @@ if (isset($_POST['login'])) {
 
     if ($result->num_rows < 1) {
 
-        $_SESSION['login_attempts']++;
-        $_SESSION['last_attempt_time'] = time();
+        // $_SESSION['login_attempts']++;
+        // $_SESSION['last_attempt_time'] = time();
         $_SESSION['error'] = 'Incorrect username or password';
     } else {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['password'])) {
             // Reset login attempts on successful login
-            $_SESSION['login_attempts'] = 0;
+            //$_SESSION['login_attempts'] = 0;
             $_SESSION['admin'] = $row['id'];
             header('location: ../sign_in.php');
             exit();
         } else {
             // Increment login attempts
-            $_SESSION['login_attempts']++;
-            $_SESSION['last_attempt_time'] = time();
+            // $_SESSION['login_attempts']++;
+            // $_SESSION['last_attempt_time'] = time();
             $_SESSION['error'] = 'Incorrect username or password';
         }
     }
