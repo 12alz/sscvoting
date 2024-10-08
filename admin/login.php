676ed756012fa051b4b804100c4d273e90192174
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-08 23:12:08 +0800

    change

--- admin/login.php
+++ admin/login.php
@@ -20,7 +20,7 @@ if (isset($_POST['login'])) {
     $nowTimestamp = time();
     $timeoutDuration = 300; // in seconds
 
-    //$attemptsCount >= 3 &&s
+    //$attemptsCount >= 3 &&shaha
     if(($nowTimestamp - $lastAttempts) < $timeoutDuration){
         $timeWait = ($nowTimestamp - $lastAttempts);
 
