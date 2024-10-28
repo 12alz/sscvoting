58446ee3f3ab9652180a7f0fe7140cc3d1163524
VillaceranJerson <villaceranjerson55@gmail.com>
2024-10-28 09:15:32 +0800

    change

--- privacy-policy.php
+++ privacy-policy.php
@@ -205,7 +205,7 @@
               <div class="row row-60">
                 <div class="col-12">
                   <h6 class="fw-bold">General Information</h6>
-                  <p>Welcome to our Privacy Policy page! When you use our , you trust us with your information. This Privacy Policy is meant to help you understand what data we collect, why we collect it, and what we do with it. When you share information with us, we can make our services even better for you. For instance, we can show you more relevant search results and ads, help you connect with people or to make sharing with others quicker and easier. As you use our services, we want you to be clear how we`re using information and the ways in which you can protect your privacy. This is important; we hope you will take time to read it carefully. Remember, you can find controls to manage your information and protect your privacy and security. We`ve tried to keep it as simple as possible.</p>
+                  <p>Welcome to our Privacy Policy page! When you use our Suprime Student Council System, you trust us with your information. This Privacy Policy is meant to help you understand what data we collect, why we collect it, and what we do with it. When you share information with us, we can make our services even better for you. For instance, we can show you more relevant search results and ads, help you connect with people or to make sharing with others quicker and easier. As you use our services, we want you to be clear how we`re using information and the ways in which you can protect your privacy. This is important; we hope you will take time to read it carefully. Remember, you can find controls to manage your information and protect your privacy and security. We`ve tried to keep it as simple as possible.</p>
                 </div>
                 <div class="col-12">
                   <h6 class="fw-bold">Right to Access, Correct and Delete Data and to Object to Data Processing</h6>
@@ -217,18 +217,18 @@
                 </div>
                 <div class="col-12">
                   <h6 class="fw-bold">Information We Collect</h6>
-                  <p>Our store collects data to operate effectively and provide you the best experiences with our services. You provide some of this data directly, such as when you create a personal account. We get some of it by recording how you interact with our services by, for example, using technologies like cookies, and receiving error reports or usage data from software running on your device. We also obtain data from third parties (including other companies). For example, we supplement the data we collect by purchasing demographic data from other companies. We also use services from other companies to help us determine a location based on your IP address in order to customize certain services to your location. The data we collect depends on the services and features you use.</p>
+                  <p>yawa kalibug.</p>
                 </div>
                 <div class="col-12">
                   <h6 class="fw-bold">How We Use Your Information</h6>
-                  <p>Our store uses the data we collect for three basic purposes: to operate our business and provide (including improving and personalizing) the services we offer, to send communications, including promotional communications, and to display advertising. In carrying out these purposes, we combine data we collect through the various store services you use to give you a more seamless, consistent and personalized experience. However, to enhance privacy, we have built in technological and procedural safeguards designed to prevent certain data combinations. For example, we store data we collect from you when you are unauthenticated (not signed in) separately from any account information that directly identifies you, such as your name, email address or phone number.</p>
+                  <p>tabang .</p>
                 </div>
                 <div class="col-12">
                   <h6 class="fw-bold">Sharing Your Information</h6>
-                  <p>We share your personal data with your consent or as necessary to complete any transaction or provide any service you have requested or authorized. For example, we share your content with third parties when you tell us to do so. When you provide payment data to make a purchase, we will share payment data with banks and other entities that process payment transactions or provide other financial services, and for fraud prevention and credit risk reduction. In addition, we share personal data among our controlled affiliates and subsidiaries. We also share personal data with vendors or agents working on our behalf for the purposes described in this statement. For example, companies we've hired to provide customer service support or assist in protecting and securing our systems and services may need access to personal data in order to provide those functions. In such cases, these companies must abide by our data privacy and security requirements and are not allowed to use personal data they receive from us for any other purpose. We may also disclose personal data as part of a corporate transaction such as a merger or sale of assets.</p>
+                  <p>ayaw kol bata paku kul</p>
                 </div>
               </div>
-              <div class="offset-top-30"><a class="btn button-primary" href="contacts.html">Contact Us</a></div>
+              <div class="offset-top-30"><a class="btn button-primary" href="contacts.php">Contact Us</a></div>
             </div>
           </div>
         </div>
