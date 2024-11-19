21bcb4ee9d9030a37996eedcff9e801c298a7b71
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-19 20:43:04 +0800

    change

--- admin/votes.php
+++ admin/votes.php
@@ -17,7 +17,7 @@
         Votes
       </h1>
       <ol class="breadcrumb">
-        <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="home.php"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Votes</li>
       </ol>
     </section>
