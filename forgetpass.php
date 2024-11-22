5923a853e093941e1f26f0a2648c661529fee93c
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-22 15:04:28 +0800

    c

--- forgetpass.php
+++ forgetpass.php
@@ -156,7 +156,7 @@ session_start();
         </div>
         <div class="right-section">
             <h2>Forgot Password</h2>
-            <form action="../admin/function" method="post">
+            <form action="../admin/function.php" method="post">
                 <label for="email">Enter your email:</label>
                 <input type="email" id="email" name="email" placeholder="example@gmail.com" required>
                 <button type="submit" name="btn-forgotpass">Send Reset Link</button>
