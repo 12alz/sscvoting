726b8037ce7d118624c651364da0dfd815b28e0a
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-22 15:52:51 +0800

    change

--- forgetpass.php
+++ forgetpass.php
@@ -156,8 +156,7 @@ session_start();
         </div>
         <div class="right-section">
             <h2>Forgot Password</h2>
-            <form action="admin/function" method="post">
-            <input type="hidden" name="token" value="<?php echo $_SESSION['token']?>">
+            <form action="admin/function.php" method="post">
                 <label for="email">Enter your email:</label>
                 <input type="email" id="email" name="email" placeholder="example@gmail.com" required>
                 <button type="submit" name="btn-forgotpass">Send Reset Link</button>
