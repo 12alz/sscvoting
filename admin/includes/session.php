52b599deeb4541fd7735782a2a38ab39f2b1a301
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-28 10:41:44 +0800

    New Changes

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,8 +3,21 @@
 	include 'includes/conn.php';
 
 	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
-		header('location: index');
+		header('location: ../sign_in');
+		exit();
 	}
+
+	$stmt = $conn->prepare("SELECT * FROM admin WHERE id = ?");
+	$stmt->bind_param('s', $_SESSION['admin']);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if($result->num_rows=0){
+		header('location: ../sign_in');
+		exit();
+	}
+
+
 
 	$sql = "SELECT * FROM admin WHERE id = '".$_SESSION['admin']."'";
 	$query = $conn->query($sql);
