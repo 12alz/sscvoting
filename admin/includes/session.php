e536d1bce42301bd7a7bfe10078150f962c31570
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-28 11:07:32 +0800

    chanes

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,7 +2,7 @@
 	session_start();
 	include 'includes/conn.php';
 
-	// Check if the user is logged in
+	
 	if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
 		header('Location: ../sign_in');
 		exit(); 
@@ -10,10 +10,9 @@
 
 	$user_id = $_SESSION['admin'];
 
-	// Prepare the query to avoid SQL injection
 	$sql = "SELECT * FROM admin WHERE id = ?";
 	$stmt = $conn->prepare($sql);
-	$stmt->bind_param("i", $user_id); // "i" means the parameter is an integer
+	$stmt->bind_param("i", $user_id); /
 	$stmt->execute();
 	$result = $stmt->get_result();
 
