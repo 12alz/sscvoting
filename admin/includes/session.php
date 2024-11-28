0da980798ecb93869c86dced8a1ce70eebfd9dd7
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-28 11:09:36 +0800

    changes

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,24 +2,15 @@
 	session_start();
 	include 'includes/conn.php';
 
-	
-	if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
+	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
 		header('Location: ../sign_in');
 		exit(); 
 	}
 
-	$user_id = $_SESSION['admin'];
+	$user = $_SESSION['admin'];
 
-	$sql = "SELECT * FROM admin WHERE id = ?";
-	$stmt = $conn->prepare($sql);
-	$stmt->bind_param("i", $user_id); /
-	$stmt->execute();
-	$result = $stmt->get_result();
-
-	// Fetch user data if available
-	$user = $result->fetch_assoc();
-
-	// Close the statement and connection
-	$stmt->close();
-	$conn->close();
+	$sql = "SELECT * FROM admin WHERE id = $user";
+	$query = $conn->query($sql);
+	$user = $query->fetch_assoc();
+	
 ?>
