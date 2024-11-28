98399ae9b0eb109b3c12f76c8efcad3cd870ec16
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-11-28 11:01:04 +0800

    c

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -4,7 +4,7 @@
 
 	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
 		header('Location: ../sign_in');
-		exit(); 
+	
 	}
 
 	$user = $_SESSION['admin'];
@@ -12,5 +12,5 @@
 	$sql = "SELECT * FROM admin WHERE id = $user";
 	$query = $conn->query($sql);
 	$user = $query->fetch_assoc();
-	exit(); 
+
 ?>
