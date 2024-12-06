cbe6ae0338ebdc4eb3c91c7797dc25dd5788a11c
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-07 00:08:10 +0800

    cc

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -5,13 +5,6 @@ $dbname = "u510162695_votesystem";
 $username = "u510162695_votesystem";
 $password = "1Votesystem";
 
-$host = "127.0.0.1";
-$dbname = "u953213971_foxrock_db";
-$username = "foxrock";
-$password = "u953213971_foxrock_db";
-
-
-
 //create connections
 	$conn = new mysqli($host, $username, $password, $dbname);
 
