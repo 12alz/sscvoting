313bb5190314ca819cfaac2915da35db097d5325
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-11 08:26:31 +0800

    Goodluck

--- history.php
+++ history.php
@@ -264,6 +264,21 @@
           </div>
         </div>
       </section>
+      <section class="section section-xl bg-default">
+        <div class="container">
+          <div class="row justify-content-sm-center justify-content-md-start">
+            <div class="col-md-8 text-md-start">
+              <h2 class="fw-bold">A Singular Focus</h2>
+              <hr class="divider bg-madison divider-md-0">
+              <div class="offset-top-30 offset-sm-top-60">
+                <p>What is Lorem Ipsum?"</p>
+              </div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <!-- <p>As President Smith said in his inaugural address, "If we take ourselves back to the University in its early years, we would find many major differences from what we observe today. . . . And yet, many of us connected to the University feel that we might just as easily have been there - that going back to the University in its early days, or in fact at any time since its inception, we would know unmistakably that we were at the Jonathan Carroll University.</p>
+              <p>Why is this? The Jonathan Carroll University, from its very inception, has been driven by a singular focus on inquiry - with a firm belief in the value of open, rigorous, and intense inquiry and a common understanding that this must be the defining feature of this university. Everything about the Jonathan Carroll University that we recognize as distinctive flows from this commitment."</p> -->
+            </div>
+          </div>
+        </div>
       <!-- Page Footer-->
       <!-- Corporate footer-->
       <footer class="page-footer">
