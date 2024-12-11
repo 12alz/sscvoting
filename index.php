96f1af80858a01629ebcef42d63d63d2210837ad
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-11 08:07:38 +0800

    changes

--- index.php
+++ index.php
@@ -147,8 +147,8 @@ header("Pragma: no-cache");
                                 <h6>About</h6>
                                 <ul class="list-unstyled offset-lg-top-20">
                                   <li><a href="history">History</a></li>
-                                  <li><a href="people">People</a></li>
-                                  <li><a href="team-member-profile.html">Team Member Profile</a></li>
+                                  <!-- <li><a href="people">People</a></li>
+                                  <li><a href="team-member-profile.html">Team Member Profile</a></li> -->
                                 </ul>
                               </li>
                               <li>
