3ed7b2cadd51c37b1fb740aa537c18c18a258a3a
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-14 00:59:10 +0800

    c

--- submit_ballot.php
+++ submit_ballot.php
@@ -3,58 +3,63 @@
 	include 'includes/slugify.php';
 
 	if(isset($_POST['vote'])){
+		// Check if at least one candidate has been selected
 		if(count($_POST) == 1){
-			$_SESSION['error'][] = 'Please vote atleast one candidate';
-		}
-		else{
+			$_SESSION['error'][] = 'Please vote for at least one candidate';
+		} else {
 			$_SESSION['post'] = $_POST;
 			$sql = "SELECT * FROM positions";
 			$query = $conn->query($sql);
 			$error = false;
 			$sql_array = array();
+
+			// Iterate through all positions
 			while($row = $query->fetch_assoc()){
-				$position = slugify($row['description']);
+				$position = slugify($row['description']); // Generate position key based on description
 				$pos_id = $row['id'];
-				if(isset($_POST[$position])){
-					if($row['max_vote'] > 2){
-						if(count($_POST[$position]) > $row['max_vote']){
+
+				// Check if the position has candidates selected
+				if(isset($_POST[$position])) {
+					$selected_candidates = $_POST[$position];
+					$max_votes = $row['max_vote']; // Get the max number of votes allowed for the position
+
+					// If multiple candidates can be selected
+					if ($max_votes > 1) {
+						// Check if the user has selected more than the allowed number of candidates
+						if (count($selected_candidates) > $max_votes) {
 							$error = true;
-							$_SESSION['error'][] = 'You can only choose '.$row['max_vote'].' candidates for '.$row['description'];
-						}
-						else{
-							foreach($_POST[$position] as $key => $values){
-								$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$values', '$pos_id')";
+							$_SESSION['error'][] = 'You can only choose ' . $max_votes . ' candidates for ' . $row['description'];
+						} else {
+							// Insert each selected candidate into the votes table
+							foreach ($selected_candidates as $candidate_id) {
+								$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) 
+								                 VALUES ('" . $voter['id'] . "', '$candidate_id', '$pos_id')";
 							}
-
 						}
-						
+					} else {
+						// If only one candidate can be selected
+						$candidate = $selected_candidates;
+						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) 
+									     VALUES ('" . $voter['id'] . "', '$candidate', '$pos_id')";
 					}
-					else{
-						$candidate = $_POST[$position];
-						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$candidate', '$pos_id')";
-					}
-
 				}
-				
 			}
 
-			if(!$error){
-				foreach($sql_array as $sql_row){
+			// If no error, execute all the vote insertions
+			if (!$error) {
+				foreach ($sql_array as $sql_row) {
 					$conn->query($sql_row);
 				}
 
 				unset($_SESSION['post']);
 				$_SESSION['success'] = 'Ballot Submitted';
-
 			}
-
 		}
 
-	}
-	else{
+	} else {
 		$_SESSION['error'][] = 'Select candidates to vote first';
 	}
 
+	// Redirect back to the home page after handling
 	header('location: home');
-
 ?>
