bfe9a5cf4a4423efdaa6dd6758db7638e2d98f6a
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-14 01:26:40 +0800

    c

--- submit_ballot.php
+++ submit_ballot.php
@@ -1,60 +1,50 @@
 <?php
-	include 'includes/session.php';
-	include 'includes/slugify.php';
+include 'includes/session.php';
+include 'includes/slugify.php';
 
-	if(isset($_POST['vote'])){
-		if(count($_POST) == 1){
-			$_SESSION['error'][] = 'Please vote atleast one candidate';
-		}
-		else{
-			$_SESSION['post'] = $_POST;
-			$sql = "SELECT * FROM positions";
-			$query = $conn->query($sql);
-			$error = false;
-			$sql_array = array();
-			while($row = $query->fetch_assoc()){
-				$position = slugify($row['description']);
-				$pos_id = $row['id'];
-				if(isset($_POST[$position])){
-					if($row['max_vote'] > 1){
-						if(count($_POST[$position]) > $row['max_vote']){
-							$error = true;
-							$_SESSION['error'][] = 'You can only choose '.$row['max_vote'].' candidates for '.$row['description'];
-						}
-						else{
-							foreach($_POST[$position] as $key => $values){
-								$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$values', '$pos_id')";
-							}
+if (isset($_POST['vote'])) {
+    if (count($_POST) == 1) {
+        $_SESSION['error'][] = 'Please vote for at least one candidate.';
+    } else {
+        $_SESSION['post'] = $_POST;
+        $sql = "SELECT * FROM positions";
+        $query = $conn->query($sql);
+        $error = false;
+        $sql_array = array();
 
-						}
-						
-					}
-					else{
-						$candidate = $_POST[$position];
-						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$candidate', '$pos_id')";
-					}
+        while ($row = $query->fetch_assoc()) {
+            $position = slugify($row['description']);
+            $pos_id = $row['id'];
 
-				}
-				
-			}
+            if (isset($_POST[$position])) {
+                $selectedCandidates = $_POST[$position]; // Get the selected candidates
 
-			if(!$error){
-				foreach($sql_array as $sql_row){
-					$conn->query($sql_row);
-				}
+                // Check if the number of selected candidates exceeds max_vote
+                if (count($selectedCandidates) > $row['max_vote']) {
+                    $error = true;
+                    $_SESSION['error'][] = 'You can only choose ' . $row['max_vote'] . ' candidates for ' . $row['description'];
+                } else {
+                    // Insert the selected candidates into the database
+                    foreach ($selectedCandidates as $candidate_id) {
+                        $sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('" . $voter['id'] . "', '$candidate_id', '$pos_id')";
+                    }
+                }
+            }
+        }
 
-				unset($_SESSION['post']);
-				$_SESSION['success'] = 'Ballot Submitted';
+        // If no errors, save the votes
+        if (!$error) {
+            foreach ($sql_array as $sql_row) {
+                $conn->query($sql_row);
+            }
 
-			}
+            unset($_SESSION['post']);
+            $_SESSION['success'] = 'Ballot Submitted';
+        }
+    }
+} else {
+    $_SESSION['error'][] = 'Please select candidates to vote for.';
+}
 
-		}
-
-	}
-	else{
-		$_SESSION['error'][] = 'Select candidates to vote first';
-	}
-
-	header('location: home');
-
+header('location: home');
 ?>
