bef89007cba8fb2827610acaba17322d0df5912e
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-14 01:42:36 +0800

    c

--- home.php
+++ home.php
@@ -193,72 +193,61 @@ function checkSwitchState() {
 }
 setInterval(checkSwitchState, 1000);
 // End Of Check ##############################################s
-$(function() {
-    // Apply custom styling to checkboxes and radio buttons
-    $('.content').iCheck({
-        checkboxClass: 'icheckbox_flat-green',
-        radioClass: 'iradio_flat-green'
-    });
+$(function(){
+	$('.content').iCheck({
+		checkboxClass: 'icheckbox_flat-green',
+		radioClass: 'iradio_flat-green'
+	});
 
-    // Handle resetting vote selection for each position
-    $(document).on('click', '.reset', function(e) {
-        e.preventDefault();
-        var desc = $(this).data('desc');
-        $('.' + desc).iCheck('uncheck');
-    });
+	$(document).on('click', '.reset', function(e){
+	    e.preventDefault();
+	    var desc = $(this).data('desc');
+	    $('.'+desc).iCheck('uncheck');
+	});
 
-    // Handle the candidate platform modal
-    $(document).on('click', '.platform', function(e) {
-        e.preventDefault();
-        $('#platform').modal('show');
-        var platform = $(this).data('platform');
-        var fullname = $(this).data('fullname');
-        $('.candidate').html(fullname);
-        $('#plat_view').html(platform);
-    });
+	$(document).on('click', '.platform', function(e){
+		e.preventDefault();
+		$('#platform').modal('show');
+		var platform = $(this).data('platform');
+		var fullname = $(this).data('fullname');
+		$('.candidate').html(fullname);
+		$('#plat_view').html(platform);
+	});
 
-    // Handling the preview button click
-    $('#preview').click(function(e) {
-        e.preventDefault();
-        var form = $('#ballotForm').serialize();
+	$('#preview').click(function(e){
+		e.preventDefault();
+		var form = $('#ballotForm').serialize();
+		if(form == ''){
+			$('.message').html('You must vote atleast one candidate');
+			$('#alert').show();
+		}
+		else{
+			$.ajax({
+				type: 'POST',
+				url: 'preview.php',
+				data: form,
+				dataType: 'json',
+				success: function(response){
+					if(response.error){
+						var errmsg = '';
+						var messages = response.message;
+						for (i in messages) {
+							errmsg += messages[i]; 
+						}
+						$('.message').html(errmsg);
+						$('#alert').show();
+					}
+					else{
+						$('#preview_modal').modal('show');
+						$('#preview_body').html(response.list);
+					}
+				}
+			});
+		}
+		
+	});
 
-        // Ensure at least one vote is selected
-        if (form == '') {
-            $('.message').html('You must vote at least one candidate');
-            $('#alert').show();
-        } else {
-            $.ajax({
-                type: 'POST',
-                url: 'preview.php',
-                data: form,
-                dataType: 'json',
-                success: function(response) {
-                    if (response.error) {
-                        var errmsg = '';
-                        var messages = response.message;
-                        for (var i in messages) {
-                            errmsg += messages[i];
-                        }
-                        $('.message').html(errmsg);
-                        $('#alert').show();
-                    } else {
-                        $('#preview_modal').modal('show');
-                        $('#preview_body').html(response.list);
-                    }
-                }
-            });
-        }
-    });
-    
-    // Handle vote switching for single selection (radio buttons)
-    $('input[type="radio"]').on('change', function() {
-        var position = $(this).attr('name');  // Name of the radio button group (position)
-        
-        // Uncheck all other radio buttons for the same position group if the user selects another candidate
-        $('input[name="' + position + '"]').not(this).iCheck('uncheck');
-    });
 });
-
 </script>
 </body>
 </html>
