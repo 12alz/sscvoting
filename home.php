11b99d0c2bc50b81647161eebbc1e12b73d00a9b
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-15 23:08:25 +0800

    c

--- home.php
+++ home.php
@@ -176,80 +176,7 @@
 <?php include 'includes/scripts.php'; ?>
 <script>
 // Check Voting Status #####################################
-let lastSwitchStatus = null;
-function checkSwitchState() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "status.php", true);
-	xhr.onload = function() {
-		if (xhr.status == 200) {
-			var response = JSON.parse(xhr.responseText);
-			if(lastSwitchStatus !== null && lastSwitchStatus != response.switch){
-				location.reload();
-			}
-			lastSwitchStatus =  response.switch;
-		}
-	};
-	xhr.send();
-}
-setInterval(checkSwitchState, 1000);
-// End Of Check ##############################################s
 $(function(){
-	$('.content').iCheck({
-		checkboxClass: 'icheckbox_flat-green',
-		radioClass: 'iradio_flat-green'
-	});
-
-	$(document).on('click', '.reset', function(e){
-	    e.preventDefault();
-	    var desc = $(this).data('desc');
-	    $('.'+desc).iCheck('uncheck');
-	});
-
-	$(document).on('click', '.platform', function(e){
-		e.preventDefault();
-		$('#platform').modal('show');
-		var platform = $(this).data('platform');
-		var fullname = $(this).data('fullname');
-		$('.candidate').html(fullname);
-		$('#plat_view').html(platform);
-	});
-
-	$('#preview').click(function(e){
-		e.preventDefault();
-		var form = $('#ballotForm').serialize();
-		if(form == ''){
-			$('.message').html('You must vote atleast one candidate');
-			$('#alert').show();
-		}
-		else{
-			$.ajax({
-				type: 'POST',
-				url: 'preview.php',
-				data: form,
-				dataType: 'json',
-				success: function(response){
-					if(response.error){
-						var errmsg = '';
-						var messages = response.message;
-						for (i in messages) {
-							errmsg += messages[i]; 
-						}
-						$('.message').html(errmsg);
-						$('#alert').show();
-					}
-					else{
-						$('#preview_modal').modal('show');
-						$('#preview_body').html(response.list);
-					}
-				}
-			});
-		}
-		
-	});
-
-});
-</script>
-<script>$(function(){
     // Initialize iCheck
     $('.content').iCheck({
         checkboxClass: 'icheckbox_flat-green',
@@ -329,6 +256,8 @@ $(function(){
     // Initialize the checkbox states when the page loads
     updateCheckboxState();
 });
+
 </script>
+<script></script>
 </body>
 </html>
