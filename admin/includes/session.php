c73865087c3a8d130ad163d39e6aa6d2ec350fa9
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-15 22:46:54 +0800

    c

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,7 +3,7 @@
 	include 'includes/conn.php';
 
 	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
-		header('Location: ../404');
+		header('Location: ../sign_in');
 		exit(); 
 	}
 
