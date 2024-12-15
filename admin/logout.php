767f9600b16360a304264529a94ee3c8719eaf43
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-15 22:59:50 +0800

    new Changes

--- admin/logout.php
+++ admin/logout.php
@@ -1,27 +1,7 @@
 <?php
-session_start();
+	session_start();
+	session_destroy();
 
-// Destroy the session data
-session_unset();
-session_destroy();
-
-// Remove the session cookie by setting the expiration date in the past
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(), 
-        '', 
-        time() - 42000, // Expire in the past
-        $params["path"], 
-        $params["https://mccsscvoting.com"], 
-        $params["secure"], 
-        $params["httponly"]
-    );
-}
-
-// Optionally, you could also remove any custom cookies that you set during login
-
-// Redirect to the homepage or login page
-header('Location: ../index');
-exit();
+	header('location: ../index');
+	exit();
 ?>
