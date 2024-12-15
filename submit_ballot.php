19052e387660d446eef2fbd63c24484e82108661
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-15 22:06:37 +0800

    c

--- submit_ballot.php
+++ submit_ballot.php
@@ -4,7 +4,7 @@
 
 	if(isset($_POST['vote'])){
 		if(count($_POST) == 1){
-			$_SESSION['error'][] = 'Please vote at least one candidate';
+			$_SESSION['error'][] = 'Please vote atleast one candidate';
 		}
 		else{
 			$_SESSION['post'] = $_POST;
@@ -12,40 +12,32 @@
 			$query = $conn->query($sql);
 			$error = false;
 			$sql_array = array();
-
 			while($row = $query->fetch_assoc()){
 				$position = slugify($row['description']);
 				$pos_id = $row['id'];
-
-				// Check if the position has votes selected
 				if(isset($_POST[$position])){
-					// Handle cases where multiple votes are allowed (max_vote > 1)
 					if($row['max_vote'] > 1){
-						// Check if the number of selected candidates exceeds max_vote
 						if(count($_POST[$position]) > $row['max_vote']){
 							$error = true;
 							$_SESSION['error'][] = 'You can only choose '.$row['max_vote'].' candidates for '.$row['description'];
-						}
-						// Ensure exactly max_vote candidates are selected
-						elseif(count($_POST[$position]) != $row['max_vote']){
-							$error = true;
-							$_SESSION['error'][] = 'You must select exactly '.$row['max_vote'].' candidates for '.$row['description'];
 						}
 						else{
 							foreach($_POST[$position] as $key => $values){
 								$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$values', '$pos_id')";
 							}
+
 						}
+						
 					}
-					// Handle case where only one candidate can be selected (max_vote == 1)
 					else{
 						$candidate = $_POST[$position];
 						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$candidate', '$pos_id')";
 					}
+
 				}
+				
 			}
 
-			// If no error occurred, submit the votes
 			if(!$error){
 				foreach($sql_array as $sql_row){
 					$conn->query($sql_row);
@@ -53,13 +45,16 @@
 
 				unset($_SESSION['post']);
 				$_SESSION['success'] = 'Ballot Submitted';
+
 			}
 
 		}
+
 	}
 	else{
 		$_SESSION['error'][] = 'Select candidates to vote first';
 	}
 
 	header('location: home');
+
 ?>
