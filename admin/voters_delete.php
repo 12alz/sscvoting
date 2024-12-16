b3e74138b8d317e835478063d0fc75055a571362
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-17 01:35:30 +0800

    c

--- admin/voters_delete.php
+++ admin/voters_delete.php
@@ -3,20 +3,20 @@
 
 	if(isset($_POST['delete'])){
 		$id = $_POST['id'];
-		$sql = "UPDATE voters SET recstat = 1 WHERE id = '$id'";
+		
+		// Update the recstat to 1 (archived) and reset the is_registered to 0 (not registered)
+		$sql = "UPDATE voters SET recstat = 1, is_registered = 0 WHERE id = '$id'";
+
 		if($conn->query($sql)){
-			$_SESSION['success'] = 'The records was successfully archived';
+			$_SESSION['success'] = 'The record was successfully archived and user registration reset.';
 		}
 		else{
 			$_SESSION['error'] = $conn->error;
 		}
 	}
 	else{
-		$_SESSION['error'] = 'An error occured during the archiving process.';
+		$_SESSION['error'] = 'An error occurred during the archiving process.';
 	}
 
 	header('location: voters');
-
-	
-
 ?>
