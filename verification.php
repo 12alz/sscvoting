98faa45dc8eceefe2aa63517f63d268b444eb8c2
JessaUngod <161490906+12alz@users.noreply.github.com>
2024-12-17 12:12:28 +0800

    New CHanges

--- verification.php
+++ verification.php
@@ -174,7 +174,7 @@ session_start(); // Start session to use session variables
         // Check if there's a session message to display
         if (isset($_SESSION['message'])) {
             $message = addslashes($_SESSION['message']);
-            if (strpos($message, 'MS365 Account email sent successfully. Please check your Outlook inbox') !== false) {
+            if (strpos($message, 'MS365 Account sent successfully. Please check your Outlook inbox!') !== false) {
                 echo "Swal.fire({
                     title: 'Success',
                     text: '$message',
