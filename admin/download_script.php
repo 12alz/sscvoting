2b1f7cd2d26f85114e316a6af4506919028e8748
JessaUngod <161490906+12alz@users.noreply.github.com>
2025-01-25 19:10:33 +0800

    Test

--- admin/download_script.php
+++ admin/download_script.php
@@ -1,7 +1,7 @@
 <?php
 // download_script.php
 
-// Define the CSV file name
+
 $filename = "template.csv";
 
 // Set headers to trigger a download
@@ -10,15 +10,12 @@ header('Content-Disposition: attachment; filename="' . $filename . '"');
 header('Pragma: no-cache');
 header('Expires: 0');
 
-// Create a file pointer connected to the output stream
 $output = fopen('php://output', 'w');
 
-// Add header row to the CSV
-fputcsv($output, ['Firstname', 'Lastname', 'Username']); 
-// Optionally, you can add sample data
-// fputcsv($output, ['Sample Firstname', 'Sample Lastname', 'Sample Username']);
 
-// Close the output stream
+fputcsv($output, ['Firstname', 'Lastname', 'Username']); 
+
+
 fclose($output);
 exit();
 ?>
