b0137ca6726f7e57c40b26e7883a219a5e606fc3
SuperMan <161490906+12alz@users.noreply.github.com>
2025-08-28 23:19:47 +0800

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,18 +1,18 @@
 <?php
 // database credential
 $host = "127.0.0.1:3306";
-$dbname = "u510162695_votesystem";
-$username = "u510162695_votesystem";
-$password = "1Votesystem";
+$dbname = "u602214709_event";
+$username = "u602214709_event1";
+$password = "Event$@13";
 
-//create connection
+//create connections
 	$conn = new mysqli($host, $username, $password, $dbname);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
-		//echo "Connection Failed";
+		//echo "Connection Faileds";
 	}
-	//echo "Connection Success";
+	//echo "Connection Successs"s;
 
 	
 ?>
